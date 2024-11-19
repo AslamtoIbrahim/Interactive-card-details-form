5419b6e7eb2d0596bfc79e344d77c5f9f81da8ba
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2024-11-19 10:05:20 +0100

    add new files

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode : 'jit',
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'fcard': "url('./images/bg-card-front.png')",
+         'bcard': "url('./images/bg-card-back.png')",
+      },
+      colors:{
+        'cusgri' : 'linear-gradient(to right,hsl(249, 99%, 64%),hsl(278, 94%, 30%))',
+        'violet-gr': 'hsl(270, 3%, 87%)',
+        'violet-gr-dr': 'hsl(279, 6%, 55%)',
+        'violet-gr-v-dr': 'hsl(278, 68%, 11%)',
+      },
+      fontFamily: {
+        'spgr': ['Space Grotesk','serif'],
+        'lfr': ['Karla','serif'],
+        'redhat': ['','serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
